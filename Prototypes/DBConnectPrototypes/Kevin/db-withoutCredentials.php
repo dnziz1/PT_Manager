4eb54493be5f355aa1f5269072b5bd70034cd74b
Kevin Hills <kh587@kent.ac.uk>
2023-11-09 13:34:15 +0000

    Upload New File

---
+++ Prototypes/DBConnectPrototypes/Kevin/db-withoutCredentials.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost:3316";
+$dbname = 'co5590';
+$user = 'root';
+$password = '';
+//$pdo = new PDO(
+//        'mysql:host=' . $host . ';dbname=' . $dbname, $user, $password
+//);
+$pdo = new PDO('mysql:host=127.0.0.1:3316; dbname=co5590','Kevin', 'NOT SHOWN');
+
+?>
