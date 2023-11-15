81263660c2d85926f9aa269caec89d448fecf40e
Billy Spiers <bjs30@kent.ac.uk>
2023-11-15 11:14:40 +0000

    Update DatabaseConnection.java

--- Prototypes/DBConnectPrototypes/Billy/DatabaseConnection.java
+++ Prototypes/DBConnectPrototypes/Billy/DatabaseConnection.java
@@ -10,21 +10,16 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseConnection extends AsyncTask<Void, Void, String> {
+    //Set database info
     private static final String DB_URL = "jdbc:mysql://localhost:3306";
     private static final String YOUR_TABLE = "message";
     private static final String USER = "root";
     private static final String PASS = "test";
 
-
-    // Process the result set or perform other database operations
-
-    //resultSet.close();
-    //statement.close();
-    //connection.close();
-
     private String result;
     private TextView resultTextView;
 
+    //Setter method to send database result to textview
     public DatabaseConnection(TextView resultTextView) {
         this.resultTextView = resultTextView;
     }
@@ -35,13 +30,11 @@ public class DatabaseConnection extends AsyncTask<Void, Void, String> {
         Statement statement = null;
         ResultSet resultSet = null;
 
+        //SQL Query
         try {
-            //Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(DB_URL, USER, PASS);
             statement = connection.createStatement();
             resultSet = statement.executeQuery("SELECT * FROM " + YOUR_TABLE);
-
-            // Process the result set or perform other database operations
 
             result = "Success";
         } catch (Exception e) {
