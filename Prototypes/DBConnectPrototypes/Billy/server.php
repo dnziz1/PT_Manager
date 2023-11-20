67c560725ac10da22a3f8ab8d3ba5378e88e1275
Billy Spiers <bjs30@kent.ac.uk>
2023-11-20 12:13:30 +0000

    Updated server.php comments.

--- Prototypes/DBConnectPrototypes/Billy/server.php
+++ Prototypes/DBConnectPrototypes/Billy/server.php
@@ -1,36 +1,37 @@
 <?php
-// server.php
-
-// Set the content type to text/plain
+//Set the content type to text/plain
 header('Content-Type: text/plain');
 
+//Database server settings
 $servername = "localhost";
 $username = "root";
 $password = "test";
 $dbname = "appDB";
 
-// Create connection
+//Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+//Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully\r\n\n";
+echo "Connected Successfully!\r\n\n";
 
 
-// Check if the 'query' parameter is set in the GET request
+//Check if the 'query' parameter is set in the GET request
 if (isset($_GET['query'])) {
+    //Get query and send it to the DB, getting the result
     $sql = $_GET['query'];
     $result = $conn->query($sql);
 
     if ($result) {
-        // Assuming a SELECT query, you can modify this based on your needs
+        //For any SELECT query, send each value into an array
         if ($result->num_rows > 0) {
             $data = array();
             while($row = $result->fetch_assoc()) {
                 $data[] = $row;
             }
+            //Display the result as json
             echo json_encode($data);
         } else {
             echo "No results";
