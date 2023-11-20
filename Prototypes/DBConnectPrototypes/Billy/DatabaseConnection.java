ddc4f1ad19c6d3b4d48997a6891e7e2497b67de5
Billy Spiers <bjs30@kent.ac.uk>
2023-11-20 12:04:34 +0000

    Update DatabaseConnection.java to connect to localhost PHP server, and pass SQL queries.

--- Prototypes/DBConnectPrototypes/Billy/DatabaseConnection.java
+++ Prototypes/DBConnectPrototypes/Billy/DatabaseConnection.java
@@ -2,52 +2,42 @@ package com.example.comp6000app;
 
 import android.os.AsyncTask;
 import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-public class DatabaseConnection extends AsyncTask<Void, Void, String> {
-    //Set database info
-    private static final String DB_URL = "jdbc:mysql://localhost:3306";
-    private static final String YOUR_TABLE = "message";
-    private static final String USER = "root";
-    private static final String PASS = "test";
-
+public class DatabaseConnection extends AsyncTask<String, Void, String> {
     private String result;
     private TextView resultTextView;
 
-    //Setter method to send database result to textview
     public DatabaseConnection(TextView resultTextView) {
         this.resultTextView = resultTextView;
     }
 
-    @Override
-    protected String doInBackground(Void... voids) {
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
+    //@Override
+    protected String doInBackground(String... params) {
+        String query = params[0];
+        String result = "";
 
-        //SQL Query
         try {
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM " + YOUR_TABLE);
+            // Encode the query parameter to handle special characters
+            String encodedQuery = URLEncoder.encode(query, "UTF-8");
 
-            result = "Success";
-        } catch (Exception e) {
-            e.printStackTrace();
-            result = "Error: " + e.getMessage();
-        } finally {
-            try {
-                if (resultSet != null) resultSet.close();
-                if (statement != null) statement.close();
-                if (connection != null) connection.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+            // Update the URL to include the query parameter
+            URL url = new URL("http://10.0.2.2:8000/server.php?query=" + encodedQuery);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result += line + "\n";
             }
+            reader.close();
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return result;
