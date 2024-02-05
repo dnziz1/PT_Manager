7142b031aeffc9fe8a39d00ff23a7d5df8bc261b
Kevin Hills <kh587@kent.ac.uk>
2024-02-05 12:29:03 +0000

    Changes to program create-edit

---
+++ pt-app/app/src/main/java/com/example/pt_app/ProgramCreateEdit.java
@@ -0,0 +1,14 @@
+package com.example.pt_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProgramCreateEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_program_create_edit);
+    }
+}
