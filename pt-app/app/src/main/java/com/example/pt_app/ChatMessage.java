71e2eb01200c81f80b1f15b2a76e4ea11c41dd61
Daniel Nkurunziza <dn247@kent.ac.uk>
2024-03-04 12:52:45 +0000

    Upload to chat feature

---
+++ pt-app/app/src/main/java/com/example/pt_app/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.example.pt_app;
+
+public class ChatMessage {
+    private String message;
+    private boolean isSentByUser; // Indicator to differentiate between received and sent messages
+
+    public ChatMessage(String message, boolean isSentByUser) {
+        this.message = message;
+        this.isSentByUser = isSentByUser;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isSentByUser() {
+        return isSentByUser;
+    }
+}
