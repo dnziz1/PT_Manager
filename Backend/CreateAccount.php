188dc1d98a90fe6710ff93c52f16b8427fd14fd2
bjs30 <billyspiers13@outlook.com>
2024-03-24 17:58:46 +0000

    Added optional inputs into account creation

--- Backend/CreateAccount.php
+++ Backend/CreateAccount.php
@@ -13,35 +13,41 @@ include_once 'DBConnection.php';
 
 $conn = connectToDB();
 
+$userID = NULL;
+$user = NULL;
+$fname = NULL;
+$lname = NULL;
+$phone = NULL;
+$email = NULL;
+
 //Required inputs
-if (isset($_GET['username']) && isset($_GET['password']) && isset($_GET['firstname']) && isset($_GET['lastname']) && isset($_GET['dob'])) {
-    $user = $_GET['username'];
-    $pass = $_GET['password'];
-    $fname = $_GET['firstname'];
-    $lname = $_GET['lastname'];
-    $dob = $_GET['dob'];
+if (isset($_GET['user']) && isset($_GET['pass']) && isset($_GET['fname']) && isset($_GET['lname'])) {
+    $user = $_GET['user'];
+    $pass = $_GET['pass'];
+    $fname = $_GET['fname'];
+    $lname = $_GET['lname'];
 
     //Optional inputs
-    //if (isset($_GET['phonenumber'])) {
-    //    $accountType = $_GET['phonenumber'];
-    //}
-    //if (isset($_GET['address'])) {
-    //    $accountType = $_GET['address'];
-    //}
+    if (isset($_GET['phonenumber'])) {
+        $phone = $_GET['phonenumber'];
+    }
+    if (isset($_GET['email'])) {
+        $email = $_GET['email'];
+    }
     
     //Searches database for username
-    $sql = "SELECT * FROM clientLoginTest WHERE username = '$user'";
+    $sql = "SELECT * FROM clients WHERE username = '$user'";
     $result = $conn->query($sql);
     //If username does not already exist
     if ($result->num_rows === 0) {
         //Create SQL query using values and execute
-        $sql = "SELECT * FROM clientLoginTest";
+        $sql = "SELECT * FROM clients";
         $result = $conn->query($sql);
         //Gets userID as number of existing users + 1
         $userID = mysqli_num_rows($result)+1;
 
         //Insert new account
-        $sql = "INSERT INTO clientLoginTest (clientID, username, password) VALUES ('$userID', '$user', '$pass')";
+        $sql = "INSERT INTO clients (cId, username, password, firstName, lastName, phoneNumber, email) VALUES ('$userID', '$user', '$pass', '$fname', '$lname', " . ($phone ? "'$phone'" : "NULL") . ", " . ($email ? "'$email'" : "NULL") . ")";
 
         if (mysqli_query($conn,$sql)) {
             echo "New account created successfully";
